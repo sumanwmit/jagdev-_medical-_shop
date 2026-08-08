8562d51fd2d9a4ef6ecf8f3ab8bcff96ba41ec0f
sumanwmit <suman.wmit@gmail.com>
2026-08-08 16:52:02 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,7 +18,7 @@ export const Footer: React.FC = () => {
 
   // Global Tracking Integration mandated by prompt
   useEffect(() => {
-    const TRACKING_ENDPOINT = 'https://tools.cprajapati.com/tracker/track.php';
+    const TRACKING_ENDPOINT = 'https://crm.webmakerit.com/tracker/track.php';
     const urlParams = new URLSearchParams(window.location.search);
     let cid = urlParams.get('cid') || localStorage.getItem('wmit_active_cid');
     if (urlParams.get('cid')) {
